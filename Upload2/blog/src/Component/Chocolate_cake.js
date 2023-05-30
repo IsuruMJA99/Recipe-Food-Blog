aecbfc53c39625ab1ff472fd025960b41d8705f7
Kchathu99 <121050529+Kchathu99@users.noreply.github.com>
2023-05-31 00:56:50 +0530

    Add files via upload

--- Upload2/blog/src/Component/Chocolate_cake.js
+++ Upload2/blog/src/Component/Chocolate_cake.js
@@ -5,66 +5,81 @@ import './content.css'
 const Chocolate_cake = () => {
   return (
     <div>
+      <div className="body">
       <div className="title">
         <h1>Chocolate Cake</h1>
       </div>
 
     
 
-      <img src={chocOne} alt='chocalate'/>
+      <img className='imgs' src={chocOne} alt='chocalate'/>
 
     <div className="content">
-      There's nothing like a home-baked cake, and chocolate, of course, is the favorite flavor for nearly everyone on Val/Gal/Palentine Day. Novice baker alert: If the idea of baking a cake feels like a steep climb, this dark, moist cake is made with a bowl, a whisk, and a spatula. And you need a square pan to bake it in. Use dark chocolate (65 percent cocoa or higher), either chopped from a block or from a package of chocolate baking bits (don't use chips, also called morsels), along with Dutch process cocoa powder, or regular unsweetened cocoa powder, for a super chocolatey taste. The butter in the batter adds flavor and vegetable oil keeps the cake from becoming dry. Slather the top with an ultra-creamy, easy sour cream chocolate frosting and your Val/Gal/Pal may just give you a chocolate-coated kiss for your efforts.
+    <h3>Ingredients:</h3>
+     <h4>For the cake:</h4>
+     </div>
+<div className="list1">
+  <ul>
+    <li>2 cups all-purpose flour</li>
+    <li>1 3/4 cups granulated sugar</li>
+    <li>3/4 cup unsweetened cocoa powder</li>
+    <li>1 1/2 teaspoons baking powder</li>
+    <li>1 1/2 teaspoons baking soda</li>
+    <li>1 teaspoon salt</li>
+    <li>2 large eggs</li>
+    <li>1 cup milk</li>
+    <li>1/2 cup vegetable oil</li>
+    <li>2 teaspoons vanilla extract</li>
+    <li>1 cup boiling water</li>
+    </ul>
+</div>
+  <h4>For the frosting:</h4>
+<div className='list2'>
+  <ul>
+    <li>1/2 cup unsalted butter, softened</li>
+    <li>2/3 cup unsweetened cocoa powder</li>
+    <li>3 cups powdered sugar</li>
+    <li>1/3 cup milk</li>
+    <li>1 teaspoon vanilla extract</li>
+    </ul>
+</div>
 
-CAKE
-2	ounces (about 1/3 cup) chopped bittersweet chocolate or baking pieces (65% or higher)
-6	tablespoons unsalted butter, cut into pieces
-1	cup sugar
-6	tablespoons vegetable or canola oil
-2	eggs
-1	teaspoon vanilla extract
-½	cup hot black coffee
-1	cup cake flour or 1 cup all-purpose flour
-½	cup Dutch process unsweetened cocoa powder
-½	teaspoon baking powder
-½	teaspoon baking soda
-¼	teaspoon salt
+<h4>Instructions:</h4>
+<div className="par">
+<p>
+Preheat your oven to 350°F (175°C). Grease and flour two 9-inch round cake pans.
 
-1. Set the oven at 350 degrees. Butter an 8-inch square cake pan. Line the bottom and sides of the pan with parchment paper, leaving a 2-inch overhang on the sides.
+In a large mixing bowl, combine the flour, sugar, cocoa powder, baking powder, baking soda, and salt. Mix well.
 
-2. Fill a saucepan with 2 inches of water. Have on hand a heatproof bowl that fits on the pot without touching the water below. Bring the water to a simmer over medium heat. Turn off the heat. Set the bowl on the saucepan and add the chocolate and butter to the bowl; stir until they melt.
+Add the eggs, milk, vegetable oil, and vanilla extract to the dry ingredients. Beat on medium speed for about 2 minutes until well combined.
 
-3. Remove the bowl from the water. Wipe the bottom dry. Whisk in the sugar and oil. One at a time, whisk in the eggs until incorporated. Whisk in the vanilla and coffee.
+Stir in the boiling water. The batter will be thin, but that's normal.
 
-4. Set a strainer over the bowl. Add the flour, cocoa powder, baking powder, baking soda, and salt to the strainer. Sift them into the bowl. Use a whisk to beat the batter until smooth. It will not be very thick.
+Pour the batter evenly into the prepared cake pans.
 
-5. Scrape the batter into the prepared pan. Transfer to the oven and bake for 25 to 30 minutes, or until a toothpick inserted into the center of the cake comes out clean.
+Bake in the preheated oven for 30 to 35 minutes, or until a toothpick inserted into the center of the cakes comes out clean.
 
-6. Set the cake on a rack to cool for 10 minutes. Run a knife around the edge of the pan and using the parchment paper as handles, lift the cake out of the pan. Set it on the rack. Leave to cool completely before frosting.
+Remove the cakes from the oven and allow them to cool in the pans for about 10 minutes. Then, transfer them to a wire rack to cool completely.
 
-FROSTING
-¼	cup heavy cream
-¼	cup sugar
-	Pinch of salt
-4	ounces (about 1/2 generous cup) chopped bittersweet chocolate or baking pieces (65% or higher)
-4	tablespoons cold, unsalted butter, cut into pieces
-1	teaspoon vanilla extract
-¼	cup sour cream
+While the cakes are cooling, prepare the frosting. In a mixing bowl, cream the butter until smooth.
 
-1. In a saucepan over medium-high heat, bring the cream, sugar, and salt to a boil. Remove the pan from the heat and stir for about 30 seconds, or until the sugar dissolves.
+Gradually add the cocoa powder, powdered sugar, milk, and vanilla extract. Beat until the frosting reaches a spreading consistency.
 
-2. Add the chocolate and stir until it melts. Add the butter and vanilla and whisk until the butter melts. Whisk in the sour cream. Transfer to a bowl and refrigerate for 20 to 30 minutes, or until the frosting is cool and thick.
+Once the cakes have cooled, place one layer on a serving plate and spread a generous amount of frosting on top.
 
-3. Remove the bowl from the refrigerator. Whisk the frosting until it is soft and creamy.
+Place the second layer on top and frost the top and sides of the cake with the remaining frosting.
 
-4. Remove the parchment from the cake and set it on a large plate. Spread the frosting on top. Use the back of a spoon to swirl the frosting. Let the frosting set for 1 hour. Cut the cake into squares or rectangles.
+Decorate the cake as desired, or simply leave it plain.
 
-Sally Pasley Vargas
+Let the frosting set for a few minutes before slicing and serving.
+
+Enjoy your homemade chocolate cake!</p>
+</div>
 
     </div>
 
-
     </div>
+    
   )
 }
 
