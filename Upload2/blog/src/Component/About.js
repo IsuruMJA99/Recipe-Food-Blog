f0a99397b536c808bc5cc7b0973384acf75435c6
Nizara <67256107+Nishara-Sewminie@users.noreply.github.com>
2023-06-12 18:18:21 +0530

    Update About.js

--- Upload2/blog/src/Component/About.js
+++ Upload2/blog/src/Component/About.js
@@ -12,9 +12,9 @@ import prof from './images/about-1.jpg';
           <img src={prof} alt="profile" className="prof" />
           <div className="hi"><b>Hi, My Name is Linda</b>
           <br/><br/>
-          And Pinch of Yum is my little corner of the internet!<br/><br/>
+          And Tipcy is my little corner of the internet!<br/><br/>
 
-I'm the voice, author, and creator behind Pinch of Yum. What started as a casual hobby over 10 years ago in 2010 while I was working as a fourth grade teacher has now grown into a full-blown business (!!) that reaches millions of people with fun recipes each month, with content that has been featured on The Kitchn, CNN, Refinery29, Brit + Co, POPSUGAR, Huffington Post, The Everymom, PureWow, and more.<br/>
+I'm the voice, author, and creator behind Tipcy. What started as a casual hobby over 10 years ago in 2010 while I was working as a fourth grade teacher has now grown into a full-blown business (!!) that reaches millions of people with fun recipes each month, with content that has been featured on The Kitchn, CNN, Refinery29, Brit + Co, POPSUGAR, Huffington Post, The Everymom, PureWow, and more.<br/>
 
 I live in Saint Paul, MN with my husband Bjork and our dog Sage. My favorite things in life are a big plate of pad Thai, sunny days, and going to the dog park.</div>
       </div>
