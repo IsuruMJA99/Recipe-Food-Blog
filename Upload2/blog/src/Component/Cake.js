fdb60bef4360901ad0dd2528d1949c58e71a4450
Kchathu99 <121050529+Kchathu99@users.noreply.github.com>
2023-06-12 20:36:20 +0530

    Cake.css and Cake.js

--- Upload2/blog/src/Component/Cake.js
+++ Upload2/blog/src/Component/Cake.js
@@ -16,21 +16,21 @@ const Cake = () => {
         <img src={chocolate} className="img" alt="chocolate-cake" />
         <div class="right-side">
           <div class="paragraph-div-content">
-            <p>a live for chocolate lovers</p>
+            a live for chocolate lovers
           </div>
           <div class="paragraph-div-title">
-            <p>The Perfect Chocolate Cake</p>
+            The Perfect Chocolate Cake
           </div>
           <div class="paragraph-div-content">
-            <p>
+          
               This Chocolate Cake recipe is perfect for that die hard chocolate
               fan in your life.
               <br />
               Homemade chocolate icing is used in the cake layers, frosting, and
               the drip!
               <br />I am obsessed with how pretty this Chocolate Cake turned
-              out. Not only the perfectly smooth cake
-            </p>
+              out. Not only the perfectly smooth cake.
+            
           </div>
 
           <Link to="/recipie/cake/chocolate-cake" className="nav-cake">
@@ -44,13 +44,13 @@ const Cake = () => {
         <img src={smores} className="img" alt="butter-cake" />
         <div class="right-side">
           <div class="paragraph-div-content">
-            <p>a live for chocolate lovers</p>
+            a live for chocolate lovers
           </div>
           <div class="paragraph-div-title">
-            <p>S’MORES Cake</p>
+            S’MORES Cake
           </div>
           <div class="paragraph-div-content">
-            <p>
+            
               This S'MORES Cake recipe is perfect for that die hard S'MORES fan
               in your life.
               <br />
@@ -58,7 +58,7 @@ const Cake = () => {
               drip!
               <br />I am obsessed with how pretty this S'MORES Cake turned out.
               Not only the perfectly smooth cake
-            </p>
+            
           </div>
 
           <Link to="/recipie/cake/smores-cake" className="nav-cake">
@@ -73,18 +73,18 @@ const Cake = () => {
         <img src={butter} className="img" alt="butter-cake" />
         <div class="right-side">
           <div class="paragraph-div-content">
-            <p>a live for butter cake lovers</p>
+            a live for butter cake lovers
           </div>
           <div class="paragraph-div-title">
-            <p>Butter Cake</p>
+            Butter Cake
           </div>
           <div class="paragraph-div-content">
-            <p>
+            
               This butter Cake recipe is perfect for that die hard butter fan in
               your life.
               <br />I am obsessed with how pretty this Butter Cake turned out.
               Not only the perfectly smooth cake
-            </p>
+            
           </div>
 
           <Link to="/recipie/cake/butter-cake" className="nav-cake">
@@ -99,13 +99,13 @@ const Cake = () => {
         <img src={redVelvet} className="img" alt="redVelvet-cake" />
         <div class="right-side">
           <div class="paragraph-div-content">
-            <p>a live for red velvet cake lovers</p>
+            a live for red velvet cake lovers
           </div>
           <div class="paragraph-div-title">
-            <p>Red Velvet Cake</p>
+            Red Velvet Cake
           </div>
           <div class="paragraph-div-content">
-            <p>
+            
               This Red Velvet Cake recipe is perfect for that die hard red
               velvet fan in your life.
               <br />
@@ -113,7 +113,6 @@ const Cake = () => {
               drip!
               <br />I am obsessed with how pretty this Red Velvet Cake turned
               out. Not only the perfectly smooth cake
-            </p>
           </div>
 
           <Link to="/recipie/cake/red-velvet-cake" className="nav-cake">
