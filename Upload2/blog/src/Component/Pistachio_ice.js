9e08b8acb63c3a0e92feb6e0c0acfa20c1f8dc47
nethmidulakshi <121050819+nethmidulakshi@users.noreply.github.com>
2023-06-12 21:55:58 +0530

    Add files via upload

--- Upload2/blog/src/Component/Pistachio_ice.js
+++ Upload2/blog/src/Component/Pistachio_ice.js
@@ -1,10 +1,69 @@
 import React from 'react'
+import pistachioOne from './images/pistacchio2.jpg';
+import './content.css'
 
 const Pistachio_ice = () => {
   return (
     <div>
-      PISTACHIO RECIPIE GOES HERE
+    <div className="body">
+    <div className="title">
+      <h1>S'mores cake</h1>
     </div>
+
+  
+
+    <img className='image' src={pistachioOne} alt='pista'/>
+
+  <div className="content">
+  <h3>Ingredients:</h3>
+   <h4>For the Ice Cream:</h4>
+   </div>
+<div className="list1">
+<ul>
+  <li>2 cups shelled roasted pistachios, divided</li>
+  <li>1 ½ cups whole milk</li>
+  <li>1 ½ cups whole milk</li>
+  <li>4 large egg yolks</li>
+  <li>1 ½ cups heavy cream</li>
+  <li>1 teaspoon vanilla extract</li>
+  <li>¼ teaspoon almond extract</li>
+  <li>Green food coloring, optionalk</li>
+  </ul>
+</div>
+
+<br></br>
+<h3>Instructions:</h3>
+<div className="par">
+<p>
+Chill the ice cream bowl for at least 24 hours or until solid. You should not hear any water sloshing around when you shake the bowl.
+In a food processor, combine 1 cup of pistachios with ¼ cup of granulated sugar. Process until finely ground.
+Add whole milk to a large saucepan and heat over medium. Add the pistachio mixture and stir to combine.
+Bring to a simmer. Once simmering, remove from heat and cover with a lid. Allow to steep for 1 hour.
+Strain the pistachios out of the mixture, pressing down as you strain to release as much flavor as possible.
+Return the milk mixture to the saucepan and again bring it to a simmer. Once it starts to simmer, remove the pan from the heat.
+In a separate medium bowl, whisk together the egg yolks and remaining granulated sugar until smooth and pale yellow in color.
+Add about ½ cup of the hot milk to the egg and sugar mixture and whisk constantly until combined.
+Then, add the tempered egg mixture back into the saucepan with the remaining hot milk.
+Heat the saucepan on low. Stirring constantly, heat until the base has thickened enough to coat the back of a spoon. This should take a few minutes.
+Strain the mixture into a bowl to remove any pieces of egg that may have cooked.
+Add the heavy cream, vanilla extract, and almond extract and stir to combine.
+Then, add green food coloring, a little at a time, until you reach your desired green coloring.
+Cover the bowl with plastic wrap making sure to have the plastic wrap touching the surface of the ice cream base. This will help keep it from forming a skin while chilling. Refrigerate for at least 3 hours, or until cold.
+Churn the ice cream mixture in the bowl of your ice cream machine according to your manufacturer’s instructions (mine took about 20 minutes), until it is thick and the consistency of soft-serve ice cream.
+In the last two minutes, add the remaining pistachios and churn until they are evenly distributed.
+Spoon the ice cream into a shallow container or a loaf pan. Press plastic wrap or wax paper to the surface of the ice cream. Freeze for at least 4 hours, or until solid. Let sit at room temperature for about 5 minutes before serving.
+</p>
+</div>
+
+  </div>
+
+
+  <div className="footer">
+      © 2023 Tipcy All Rights Reserved. An elite Dessert food
+      publisher.
+    </div>
+
+  </div>
   )
 }
 
